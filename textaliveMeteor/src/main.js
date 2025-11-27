5497726d486779fe405529dc5553242ca33ca6c8
Aiii-1111 <231912963+Aiii-1111@users.noreply.github.com>
2025-11-27 14:40:06 +0000

    initial commit
    scaffold vite project and import textalive api

---
+++ textaliveMeteor/src/main.js
@@ -0,0 +1,9 @@
+import { Player } from "textalive-app-api";
+
+//Get HTML objects
+
+//create methods & listeners
+
+//Initialise player
+const player = new Player({app:{token:"ToQM0IhgEahcXQEo"},
+mediaElement: document.querySelector("media")});
